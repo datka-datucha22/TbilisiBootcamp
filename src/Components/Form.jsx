7eb88bfbe5b291285a83964d750fdfcf23890675
datka-datucha22 <102787951+datka-datucha22@users.noreply.github.com>
2022-09-14 14:15:25 +0400

    form DONE

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -20,15 +20,21 @@ export const ContactUs = () => {
         <form ref={form} onSubmit={sendEmail} className='bootcamp-form'>
 
             <label className="form-name-label">სახელი</label>
-            <input className="form-name-input" placeholder="სახელი" type="text" name="to_name" />
+            <input className="form-name-input form-all-input" placeholder="სახელი" type="text" name="name" />
 
             <label className="form-name-label">გვარი</label>
-            <input className="form-name-input" placeholder="გვარი" type="text" name="to_name" />
+            <input className="form-name-input" placeholder="გვარი" type="text" name="lastname" />
 
-            <label className="form-email-label">მეილი@</label>
-            <input className="form-email-input" type="email" placeholder="მეილი" name="from_name" />
-            <label className="form-message-label">მესიჯი</label>
+            <label className="form-age-label">ასაკი</label>
+            <input className="form-age-input" type="number" placeholder="ასაკი" name="age" />
+
+            <label className="form-email-label">მეილი</label>
+            <input className="form-email-input" type="email" placeholder="მეილი" name="mail" />
+
+            <label className="form-number-label">ნომერი</label>
             <input className="form-number-input" type="number" name="number" placeholder="ნომერი" />
+
+
             <input className="form-button-input" type="submit" value="გაგზავნა" />
         </form >
     );
