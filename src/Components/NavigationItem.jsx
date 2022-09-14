dd570b93dd60367572c14ad29ca3444371bdc75e
Luka Koridze <luka.qoridze2002@gmail.com>
2022-09-14 13:43:17 +0400

    Navigation Style Change

--- src/Components/NavigationItem.jsx
+++ src/Components/NavigationItem.jsx
@@ -6,10 +6,10 @@ export default function NavigationItem(props) {
         <NavLink to={props.path}>
             <div className="nav-item"
                 onMouseEnter={(e) => { e.target.children[0].src = props.hover }}
-                onMouseLeave={(e) => { e.target.children[0].src = props.icon }}>
-                <img src={props.icon} alt={props.go} />
-                <p>{props.go}</p>
-            </div>
-        </NavLink>
+                onMouseLeave={(e) => { e.target.children[0].src = props.icon }} >
+            <img src={props.icon} alt={props.go} />
+            <p>{props.go}</p>
+        </div>
+        </NavLink >
     )
 }
