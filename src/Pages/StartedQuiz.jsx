ae477046a32915b6cf2bef4253dfcbefbc55f587
Luka Koridze <luka.qoridze2002@gmail.com>
2022-09-14 15:28:45 +0400

    Find Quiz

--- src/Pages/StartedQuiz.jsx
+++ src/Pages/StartedQuiz.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
-
+import quizes from '../Quizes.json'
 export default function StartedQuiz() {
     let {id} = useParams()
+    const [quiz,setQuiz] = useState(null)
+    if(quiz===null){
+      quizes.map((item) => {
+        if (item.id === id) {
+          setQuiz(item)
+        }
+      })
+    }
   return (
-    <div>{id}</div>
+    <div className="startedQuiz">
+      <div className="top">
+        <h1>{quiz.topic}</h1>
+      </div>
+    </div>
   )
 }
